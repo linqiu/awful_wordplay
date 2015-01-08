1d0f58e0f55742c6ce4ee2fb6d69f3d50b482b2e
Lin Qiu <lin@kitcheck.com>
2015-01-07 21:43:00 -0500

    Add result
    - switch form inputs out into a directive to add more nuisance to
    error messaging
    - add more states for error retries
    - fixed url encoding error

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,10 +1,14 @@
 class WordsController < ApplicationController
   def show
-    word = params[:word]
+    word = url_encode(params[:word])
     token = 'q4iFO4Y6lFg3jlH9qJ5zZZVgFS4'
     url = 'https://www.wordsapi.com/words'
 
     response = HTTParty.get("#{url}/#{word}/definitions?accessToken=#{token}")
     render json: response.body
+  end
+
+  def url_encode(word)
+    CGI::escape(word)
   end
 end
